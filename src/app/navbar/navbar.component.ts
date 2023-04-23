c243b107eee289abdde82e978a2b1ec46180df86
mintuka <mintuka2015@gmail.com>
2023-04-23 08:36:08 +0300

    Refactored navbar and footer

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -9,10 +9,31 @@ import { AuthService } from '../services/auth.service';
 })
 export class NavbarComponent {
   isVisible = false;
+  homeColor = 'yellow'
+  cartColor = 'white'
   user: any;
   constructor (private auth: AuthService, private router: Router) {
     if ( localStorage.getItem('user') !== null )
       this.user = JSON.parse(localStorage.getItem('user') || '')
+    
+    const currentUrl = this.router.url;
+    const path = currentUrl.split('/')
+    if ( path && path[path.length-1] === 'cart' ) {
+      this.homeColor = 'white'
+      this.cartColor = 'yellow'
+    }
+  }
+
+  changeHomeColor () {
+    console.log('hello home')
+    this.homeColor = 'yellow'
+    this.cartColor = 'white'
+  }
+
+  changeCartColor () {
+    console.log('hello cart')
+    this.cartColor = 'yellow'
+    this.homeColor = 'white'
   }
 
   isLoggedIn = () => {
